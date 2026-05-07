c0079ba550e8ccc79787e996befd3aa6e9dc2b61
Sneha Singla <152980805+snehasingla@users.noreply.github.com>
2026-05-07 14:55:55 +0530

    Add: house-robber-iii - submission-1

---
+++ Data Structures & Algorithms/house-robber-iii/submission-1.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ //dp solution memoization
+public class Solution {
+    private Map<TreeNode, Integer> cache;
+
+    public int rob(TreeNode root) {
+        cache = new HashMap<>();
+        cache.put(null, 0);
+        return dfs(root);
+    }
+
+    private int dfs(TreeNode root) {
+        if (cache.containsKey(root)) {
+            return cache.get(root);
+        }
+
+        int res = root.val;
+        if (root.left != null) {
+            res += dfs(root.left.left) + dfs(root.left.right);
+        }
+        if (root.right != null) {
+            res += dfs(root.right.left) + dfs(root.right.right);
+        }
+
+        res = Math.max(res, dfs(root.left) + dfs(root.right));
+        cache.put(root, res);
+        return res;
+    }
+}   
